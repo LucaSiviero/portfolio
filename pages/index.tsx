848d579cb286c4719b07bbd192ecb6eb22179e57
Luca Siviero <lucasivi.998@gmail.com>
2022-11-10 20:58:25 +0100

    Changing last things

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,7 +31,7 @@ const Home: React.FC<HomeProps> = ({ page, error }) => {
       {pageOk && (
         <>
           <Head>
-            <title>{'Luca Siviero'}</title>
+            <title>Luca Siviero</title>
             <meta name="description" content={page.meta.description} />
           </Head>
           <PageViewer page={pageOk} />
